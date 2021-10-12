0e4e77aeb4cc1d75d72e215cfb6a4af124961689
rimelJB <rimel1jabnoun@gmail.com>
2021-10-12 16:41:22 +0100

    2nd commit

--- src/Components/Description/Description.js
+++ src/Components/Description/Description.js
@@ -1,55 +1,36 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import { Card, Container, Row, Col } from "react-bootstrap";
-import ReactPlayer from 'react-player'
+import { Card,Button } from "react-bootstrap";
+import ReactPlayer from "react-player";
 
-// import "./Description.css";
 
 const Description = (props) => {
   const movie = props.location.state.movie;
 
   return (
-    <>
-      <Card>
-        <Container className="container">
-          <Row>
-            <Card.Header>
-              <Card.Title>
-                <h1>{movie.name}</h1>
-              </Card.Title>
-            </Card.Header>
-          </Row>
-          <Row>
-            <Col sm={7} style={{ position: "relative" }}>
-              <div>
-                <Card.Body>
-                <ReactPlayer url={movie.trailer} />
-                </Card.Body>
-              </div>
-            </Col>
-            <Col sm={5} style={{ position: "relative" }}>
-              <div className="description-layout">
-                <div>
-                  <h3>Description</h3>
-                  <hr />
-                  <h4>{movie.description}</h4>
-                </div>
-              </div>
-              <div className="return-home-btn">
-                <Link to="/">
-                  <img
-                    className="return-home-logo"
-                    src="https://cdn-icons-png.flaticon.com/512/271/271220.png"
-                    alt="..."
-                    style={{ width: "100px" }}
-                  />
+  
+      <Card className="text-center">
+        <Card.Header>
+          {" "}
+          <h1>{movie.name}</h1>
+        </Card.Header>
+        <Card.Body>
+          <Card.Title>
+            {" "}
+            <ReactPlayer style={{marginLeft:'28%'}} url={movie.trailer} />
+          </Card.Title>
+          <Card.Text>
+            <div>
+              <h3>Description</h3>
+              <hr />
+              <p>{movie.description}</p>
+            </div>
+          </Card.Text>
+          <Link to="/">
+          <Button variant="primary">Go back </Button>
                 </Link>
-              </div>
-            </Col>
-          </Row>
-        </Container>
+        </Card.Body>
       </Card>
-    </>
   );
 };
 
